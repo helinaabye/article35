8cec3b93158a12f1bf1b0b1a75dd9257fab23a5e
Helina A. Gebreyes <helinaago@gmail.com>
2023-04-04 21:50:15 +0300

    add folders and files for frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Landing from './Pages/Landing';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: purple[500],
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: '#11cb5f',
+    },
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+    <div className="App">
+      <Landing/>
+    </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
