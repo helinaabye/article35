306eadc68d72a312e1dd42618b00fd3e29d3bd35
Tesfay Teshome <82706469+Tesfay-Teshome@users.noreply.github.com>
2023-04-11 06:30:58 +0000

    Blog database created

--- backend/models/blogs.py
+++ backend/models/blogs.py
@@ -1,8 +1,13 @@
-from models.base import BaseModel, Base
-from sqlalchemy import SQLAlchemy
-from sqlalchemy.ext.declarative import declarative_base
+from models.base import BaseModel
+from models.base import Base
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 class Blogs(BaseModel, Base):
     __tablename__ = 'blogs'
-    user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
-    
+    id = Column(Integer(150), nullabel=True)
+    user_id = Column(Integer(150), ForeignKey('users.id'), primary_key=True, nullable=False)    
+    title = Column(String(200), nullable=False)
+    blog_data= Column(String(5000), nullable=False)
+    image_url = Column(String(1000), nullable=False)
+    checked = Column(String(150), nullable=False)
