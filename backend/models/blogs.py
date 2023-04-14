1f361b594fa879b9b249ff9cc6b8fbf0bac5e225
Tesfay-Teshome <tesfix2017@gmail.com>
2023-04-14 13:53:25 +0300

    blog model added

--- backend/models/blogs.py
+++ backend/models/blogs.py
@@ -1,20 +1,13 @@
-#!/usr/bin/python3
-"""
-file: base.py
-Desc: Base model which will be shared among other models
-Authors: Gizachew Bayness, Tesfay Teshome, Helina Gebryes, Bruk Gelelcha
-Date Created: April 07 2023
-"""
-from flask import Flask
+from app import db
 from models.base import BaseModel
-from models.base import Base
-from sqlalchemy import Column, String, Integer, ForeignKey
-from sqlalchemy.orm import relationship
+from models.user import User
 
-class Blogs(BaseModel, Base):
-    __tablename__ = 'blogs'
-    user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
-    title = Column(String(200), nullable=False)
-    blog_data= Column(String(5000), nullable=False)
-    image_url = Column(String(1000), nullable=False)
-    checked = Column(String(150), nullable=False)
+class Blog(db.Model, BaseModel):
+	__tablename__ = 'blogs'
+    title = db.Column(db.String(150), nullable=False)
+    blogData = db.Column(db.String(20000), nullable=False)
+    imageUrl = db.Column(db.String(10000), nullable=False)
+    checked = db.Column(db.Boolean, nullable=False)
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('user_id'), nullable=False)
+    user = db.relationship('User', backref='blogs')
