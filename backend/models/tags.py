f55397068cb0fe1486b32ccd5dcca454c819ca73
Tesfay-Teshome <tesfix2017@gmail.com>
2023-04-14 14:16:13 +0300

    clean directory

--- backend/models/tags.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""
-file: base.py
-Desc: Base model which will be shared among other models
-Authors: Gizachew Bayness, Tesfay Teshome, Helina Gebryes, Bruk Gelelcha
-Date Created: April 07 2023
-"""
