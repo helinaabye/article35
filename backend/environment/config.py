cb8256e84e0aaf52ae65b4ea3373b0b2e981da41
Gizachew Bayness <83591565+eleccrazy@users.noreply.github.com>
2023-04-20 15:32:21 +0300

    backend directory deleted to refactor the folder structure.

--- backend/environment/config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-db_URI = os.getenv('DATABASE_URL', 'postgres://localhost:5432/article35_db')
-secret = os.getenv('SECRET', 'thisisarticle35_dbsecretkey.')
--- backend/serializers/base.py
+++
@@ -1,8 +0,0 @@
-from datetime import *
-from marshmallow import fields
-from flask_marshmallow import Marshmallow 
-
-class BaseSchema:
-
-	created_at = fields.DateTime(format='%d-%m-%Y %H:%M:%S')
-	updated_at = fields.DateTime(format='%d-%m-%Y %H:%M:%S')
